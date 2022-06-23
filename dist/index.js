05e7c5fea297806321afc4ad186edff39a05f17c
Harsh Raj <harshraj8843pro@gmail.com>
2022-06-23 20:33:23 +0530

    add maintainers data

--- dist/index.js
+++ dist/index.js
@@ -35480,6 +35480,128 @@ async function collectOrgStats(owner, token) {
       console.log(`=> ${statsFilePath} succesfully saved !!!`);
     });
 
+    // humans.txt data
+    const humansAdded = [];
+    var humans = "";
+    // add logo to humans.txt
+    // ...
+    // add organisation details to humans.txt
+    humans =
+      humans +
+      `\n\n
+    /* PROJECT */
+    Site Name:   Codinasion
+    Site URL:    https://codinasion.vercel.app
+    Created:     2022-01-26
+    Web Design:  Harsh Raj @ Codinasion
+    `;
+    // add meta details to humans.txt
+    humans =
+      humans +
+      `\n\n
+    /* META */
+    Title:       Codinasion
+    Description: An Open Source community, dedicated to Open Source projects. Codinasion is a community of developers and coders.
+    Built with:  Nextjs, Docsearch, Github API, Material-UI, Giscus, and many more.
+    `;
+    // add social media details to humans.txt
+    humans =
+      humans +
+      `\n\n
+    /* SOCIAL */
+    Website:     https://codinasion.vercel.app
+    Github:      https://github.com/codinasion
+    Twitter:     https://twitter.com/codinasion
+    `;
+    // add maintainers details to humans.txt
+    humans =
+      humans +
+      `\n\n
+    /* MAINTAINERS */
+    `;
+    var maintainers_data = [];
+    var maintainers = [];
+    await fetch(
+      `https://api.github.com/organizations/98682602/team/6258326/members`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `token ${token} `,
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(
+            `The HTTP status of the reponse: ${res.status} (${res.statusText})`
+          );
+        }
+      })
+      .then((json) => {
+        maintainers_data = json;
+      })
+      .catch((err) => console.log(err));
+    // iterate through maintainers data and add to maintainers array
+    for (let i = 0; i < maintainers_data.length; i++) {
+      maintainers.push(maintainers_data[i].login);
+    }
+    // add maintainers to humans.txt
+    for (let i = 0; i < maintainers.length; i++) {
+      var maintainer_data = {};
+      // get user data from github api
+      await fetch(`https://api.github.com/users/${maintainers[i]}`, {
+        method: "GET",
+        headers: {
+          Authorization: `token ${token} `,
+        },
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error(
+              `The HTTP status of the reponse: ${res.status} (${res.statusText})`
+            );
+          }
+        })
+        .then((json) => {
+          maintainer_data = json;
+        })
+        .catch((err) => console.log(err));
+      // add maintainer to humans.txt
+      humans =
+        humans +
+        `\n
+      ${
+        maintainer_data.name !== ""
+          ? maintainer_data.name
+          : maintainer_data.login
+      }"}
+      ${
+        maintainer_data.html_url !== ""
+          ? maintainer_data.html_url
+          : "https://github.com/orgs/codinasion/people"
+      }
+      ${
+        maintainer_data.twitter_username !== "" &&
+        "https://twitter.com/" + maintainer_data.twitter_username
+      }
+      `;
+      humansAdded.push(maintainer_data.login);
+    }
+
+    await console.log(humans);
+
+    // // write humans.txt data to file
+    // const humansFileDir = "data";
+    // const humansFilePath = `${humansFileDir}/${"humans"}.txt`;
+    // await fs.writeFile(humansFilePath, humans, (err) => {
+    //   if (err) throw err;
+    //   console.log(`=> ${humansFilePath} succesfully saved !!!`);
+    // });
+
     //   collection complete
   } catch (error) {
     await console.log(`error occured !!! for ${owner} stats collect`);
