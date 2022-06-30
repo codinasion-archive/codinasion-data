46eaa38763d7a4c14fe2a0e05de15e78a0014d69
Harsh Raj <harshraj8843pro@gmail.com>
2022-06-30 18:14:33 +0530

    try dynamic function

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import collectTagData from "./scripts/tag/collectTagData";
 import collectOrgStats from "./scripts/stats/collectOrgStats";
 import collectAllDsaData from "./scripts/dsa/collectAllDsaData";
 import collectDsaData from "./scripts/dsa/collectDsaData";
-import collectQuoteData from "./scripts/quote/collectQuoteData";
-import collectQuoteCategoryData from "./scripts/quote/collectQuoteCategoryData";
 
 const core = require("@actions/core");
 
@@ -22,14 +20,10 @@ const core = require("@actions/core");
     const programmeBranch = await core.getInput("programme-branch");
     const dsaRepo = await core.getInput("dsa-repo");
     const dsaBranch = await core.getInput("dsa-branch");
-    const quoteRepo = await core.getInput("quote-repo");
-    const quoteBranch = await core.getInput("quote-branch");
     const collectProgramme = await core.getInput("collect-programme");
     const processProgramme = await core.getInput("process-programme");
     const collectDsa = await core.getInput("collect-dsa");
     const processDsa = await core.getInput("process-dsa");
-    const collectQuote = await core.getInput("collect-quote");
-    const processQuote = await core.getInput("process-quote");
     const collectTag = await core.getInput("collect-tag");
     const processTag = await core.getInput("process-tag");
     const collectStats = await core.getInput("collect-stats");
@@ -60,14 +54,6 @@ const core = require("@actions/core");
 
     if (collectStats === "true") {
       await collectOrgStats(owner, token, PAT);
-    }
-
-    if (collectQuote === "true") {
-      await collectQuoteData(owner, token, quoteRepo, quoteBranch);
-    }
-
-    if (processQuote === "true") {
-      await collectQuoteCategoryData(token);
     }
 
     // end of action
--- scripts/formatTag.js
+++ scripts/formatTag.js
@@ -1,73 +1,12 @@
-export default function formatTag(tag) {
-  if (tag === "c" || tag === "C") {
-    return {
-      tag: "c",
-      label: "C",
-    };
-  }
-  if (tag === "c++" || tag === "C++" || tag === "cpp" || tag === "CPP") {
-    return {
-      tag: "cpp",
-      label: "C++",
-    };
-  }
-  if (tag === "cs" || tag === "c#" || tag === "CS" || tag === "C#") {
-    return {
-      tag: "cs",
-      label: "C#",
-    };
-  }
-  if (tag === "java" || tag === "Java" || tag === "JAVA") {
-    return {
-      tag: "java",
-      label: "Java",
-    };
-  }
-  if (
-    tag === "py" ||
-    tag === "PY" ||
-    tag === "python" ||
-    tag === "Python" ||
-    tag === "PYTHON"
-  ) {
-    return {
-      tag: "python",
-      label: "Python",
-    };
-  }
-  if (tag === "GO" || tag === "go" || tag === "golang" || tag === "Go") {
-    return {
-      tag: "go",
-      label: "GO",
-    };
-  }
-  if (
-    tag === "js" ||
-    tag === "JS" ||
-    tag === "Js" ||
-    tag === "javascript" ||
-    tag === "JavaScript" ||
-    tag === "JAVASCRIPT"
-  ) {
-    return {
-      tag: "js",
-      label: "JS",
-    };
-  }
-  if (tag === "php" || tag === "PHP" || tag === "Php") {
-    return {
-      tag: "php",
-      label: "PHP",
-    };
-  }
-  if (tag === "julia" || tag === "Julia" || tag === "JULIA") {
-    return {
-      tag: "julia",
-      label: "Julia",
-    };
-  }
-  return {
-    tag: tag,
-    label: tag,
-  };
+export default async function formatTag(tag) {
+
+  const fnText = await fetch(
+    `https://raw.githubusercontent.com/codinasion/codinasion/master/script/formatTag.js`
+  )
+    .then((res) => res.text())
+    .catch((error) => console.log(error));
+  
+    eval(fnText);
+
+  return formatTag(tag);
 }
