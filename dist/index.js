fc6419a156e77fb85027f9bc698bb6365de80782
Harsh Raj <harshraj8843pro@gmail.com>
2022-07-01 13:26:15 +0530

    update script

--- dist/index.js
+++ dist/index.js
@@ -36124,6 +36124,8 @@ async function collectAllDsaData(
 
 
 
+
+
 async function collectDsaData(owner, token, dsaRepo, dsaBranch) {
   const dsaList = await fetch(
     `https://raw.githubusercontent.com/${"codinasion"}/${"codinasion-data"}/master/data/dsa/${"dsaList"}.json`,
@@ -36144,20 +36146,88 @@ async function collectDsaData(owner, token, dsaRepo, dsaBranch) {
       dsaList.map(async (data) => {
         const slug = data.slug;
 
-        try {
-          const source = await fetch(
-            `https://raw.githubusercontent.com/${owner}/${dsaRepo}/${dsaBranch}/programme/${slug}/README.md`,
-            {
-              method: "GET",
-              headers: {
-                Authorization: `token ${token}`,
-              },
-            }
-          )
-            .then((res) => res.text())
-            .catch((error) => console.log(error));
+        // get README.md text data
+        const readme_text = await fetch(
+          `https://raw.githubusercontent.com/${owner}/${dsaRepo}/${dsaBranch}/programme/${slug}/README.md`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `token ${token}`,
+            },
+          }
+        )
+          .then((res) => res.text())
+          .catch((error) => console.log(error));
 
-          const matterResult = await gray_matter_default()(source);
+        // get code data
+        var code_text = `
+<CodeBlock>
+        `;
+        const programme_tags = [];
+        const programme_files = await fetch(
+          `https://api.github.com/repos/${owner}/${dsaRepo}/contents/programme/${slug}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `token ${token}`,
+            },
+          }
+        )
+          .then((res) => res.json())
+          .catch((error) => console.log(error));
+
+        programme_files &&
+          (await Promise.all(
+            programme_files.map(async (file) => {
+              if (!file.path.endsWith(".md") && !file.path.endsWith(".png")) {
+                // get tags data
+                // check if tag is already in the list
+                if (
+                  !programme_tags.find((tag) => tag === file.path.split(".")[1])
+                ) {
+                  await programme_tags.push(file.path.split(".")[1]);
+                }
+
+                // get code file data
+                const response_text = await fetch(
+                  `https://raw.githubusercontent.com/${owner}/${dsaRepo}/${dsaBranch}/${file.path}`,
+                  {
+                    method: "GET",
+                    headers: {
+                      Authorization: `token ${token}`,
+                    },
+                  }
+                )
+                  .then((res) => res.text())
+                  .catch((error) => console.log(error));
+
+                code_text =
+                  code_text +
+                  `
+${"```"}${formatTag(file.path.split(".")[1]).tag}
+${response_text}
+${"```"}
+                `;
+              }
+            })
+          ));
+
+        code_text =
+          code_text +
+          `
+</CodeBlock>
+          `;
+
+        try {
+          const source = `${readme_text}
+${code_text}
+        `;
+
+          // await console.log(source);
+
+          const matterResult = await gray_matter_default()(String(source));
+
+          // await console.log(matterResult);
 
           const processedContent = await remark()
             .use(remarkHtml)
@@ -36169,7 +36239,7 @@ async function collectDsaData(owner, token, dsaRepo, dsaBranch) {
             let json_res = [];
             latestUpdateDate = await fetch(
               `https://api.github.com/repos/${owner}/${dsaRepo}/commits?path=${
-                "programme/" + slug + "/README.md"
+                "programme/" + slug
               }&page=1&per_page=1`,
               {
                 method: "GET",
@@ -36183,7 +36253,7 @@ async function collectDsaData(owner, token, dsaRepo, dsaBranch) {
               .then((json) => json[0].commit.committer.date)
               .catch((error) => console.log(slug, json_res, error));
           } catch (error) {
-            latestUpdateDate = new Date().toISOString();
+            latestUpdateDate = await new Date().toISOString();
             await console.log(
               "latestUpdateDate set to null !!! for " +
                 `https://api.github.com/repos/${owner}/${dsaRepo}/commits?path=${
@@ -36194,19 +36264,17 @@ async function collectDsaData(owner, token, dsaRepo, dsaBranch) {
           }
 
           const dsaData = JSON.stringify({
-            frontMatter: {
-              slug: slug || null,
-              title: matterResult.data.title
-                ? matterResult.data.title
-                : "Codinasion",
-              description: matterResult.data.description
-                ? matterResult.data.description
-                : "Codinasion",
-              tags: matterResult.data.tags ? matterResult.data.tags : [],
-              contributors: matterResult.data.contributors
-                ? matterResult.data.contributors
-                : [],
-            },
+            slug: slug || null,
+            title: matterResult.data.title
+              ? matterResult.data.title
+              : "Codinasion",
+            description: matterResult.data.description
+              ? matterResult.data.description
+              : "Codinasion",
+            tags: programme_tags ? programme_tags : [],
+            contributors: matterResult.data.contributors
+              ? matterResult.data.contributors
+              : [],
             latestUpdateDate: latestUpdateDate,
             contentHtml: contentHtml,
             markdown: matterResult.content,
