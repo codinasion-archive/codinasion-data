42411bce3a76141c1039adccbdef82be970e2e96
Harsh Raj <harshraj8843pro@gmail.com>
2022-07-16 23:24:49 +0530

    try: collect project data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// import programme functions
 import collectProgrammesData from "./scripts/programme/collectProgrammesData";
 import collectProgrammeData from "./scripts/programme/collectProgrammeData";
+
+// import tags functions
 import collectTagsData from "./scripts/tag/collectTagsData";
 import collectTagData from "./scripts/tag/collectTagData";
+
+// import stats functions
 import collectOrgStats from "./scripts/stats/collectOrgStats";
+
+// import dsa functions
 import collectAllDsaData from "./scripts/dsa/collectAllDsaData";
 import collectDsaData from "./scripts/dsa/collectDsaData";
+
+// import humans functions
 import generateHumansFn from "./scripts/humans/generateHumans";
+
+// import project functions
+import collectProjectsData from "./scripts/project/collectProjectsData";
 
 const core = require("@actions/core");
 
@@ -14,22 +26,38 @@ const core = require("@actions/core");
   try {
     console.log("Hii there !!!");
 
+    // default data
     const owner = await core.getInput("owner");
     const token = await core.getInput("token");
     const PAT = await core.getInput("PAT");
+
+    // programme data
     const programmeRepo = await core.getInput("programme-repo");
     const programmeBranch = await core.getInput("programme-branch");
-    const dsaRepo = await core.getInput("dsa-repo");
-    const dsaBranch = await core.getInput("dsa-branch");
     const collectProgramme = await core.getInput("collect-programme");
     const processProgramme = await core.getInput("process-programme");
+
+    // dsa data
+    const dsaRepo = await core.getInput("dsa-repo");
+    const dsaBranch = await core.getInput("dsa-branch");
     const collectDsa = await core.getInput("collect-dsa");
     const processDsa = await core.getInput("process-dsa");
+
+    // tag data
     const collectTag = await core.getInput("collect-tag");
     const processTag = await core.getInput("process-tag");
+
+    // stats data
     const collectStats = await core.getInput("collect-stats");
+
+    // humans data
     const generateHumans = await core.getInput("generate-humans");
 
+    // home data
+    const projectTopic = await core.getInput("project-topic");
+    const collectProject = await core.getInput("collect-project");
+
+    // programme conditions
     if (collectProgramme === "true") {
       await collectProgrammesData(owner, token, programmeRepo, programmeBranch);
     }
@@ -38,6 +66,7 @@ const core = require("@actions/core");
       await collectProgrammeData(owner, token, programmeRepo, programmeBranch);
     }
 
+    // dsa conditions
     if (collectDsa === "true") {
       await collectAllDsaData(owner, token, dsaRepo, dsaBranch);
     }
@@ -46,6 +75,7 @@ const core = require("@actions/core");
       await collectDsaData(owner, token, dsaRepo, dsaBranch);
     }
 
+    // tag conditions
     if (collectTag === "true") {
       await collectTagsData(owner, token);
     }
@@ -54,12 +84,19 @@ const core = require("@actions/core");
       await collectTagData(owner, token);
     }
 
+    // stats conditions
     if (collectStats === "true") {
       await collectOrgStats(owner, token);
     }
 
+    // humans conditions
     if (generateHumans === "true") {
       await generateHumansFn(owner, token, PAT);
+    }
+
+    // project conditions
+    if (collectProject === "true") {
+      await collectProjectsData(owner, token, projectTopic);
     }
 
     // end of action
---
+++ scripts/home/collectProjectData.js
@@ -0,0 +1,62 @@
+import fetch from "node-fetch";
+
+export default async function collectProjectsData(owner, token, projectTopic) {
+  try {
+    // get all projects
+    const projectsData = [];
+    const projects = await fetch(
+      `https://api.github.com/search/repositories?q=topic:${projectTopic}&sort=stars&order=desc`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `token ${token} `,
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(
+            `The HTTP status of the reponse: ${res.status} (${res.statusText})`
+          );
+        }
+      })
+      .then((json) => {
+        return json;
+      })
+      .catch((err) => console.log(err));
+
+    //   add projects to projectsData
+    projects &&
+      (await Promise.all(
+        projects.map(async (project) => {
+          if (project.owner.login === owner) {
+            const data = {
+              name: project.name,
+              description: project.description,
+              url: project.html_url,
+              stars: project.stargazers_count,
+              language: project.language,
+              owner: project.owner.login,
+              ownerUrl: project.owner.html_url,
+              ownerAvatar: project.owner.avatar_url,
+              ownerType: project.owner.type,
+              ownerCompany: project.owner.company,
+              ownerLocation: project.owner.location,
+              ownerBlog: project.owner.blog,
+              ownerBio: project.owner.bio,
+            };
+            projectsData.push(data);
+          }
+        })
+      ));
+
+    await console.log(projectsData);
+
+    //   collection complete
+  } catch (error) {
+    await console.log(`error occured !!! for ${owner} project collection !!!`);
+    await console.log(error);
+  }
+}
