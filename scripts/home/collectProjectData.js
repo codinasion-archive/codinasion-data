f938a50f73142cd84a28639b9db7d0f8550a1f99
Harsh Raj <harshraj8843pro@gmail.com>
2022-07-16 23:33:25 +0530

    try: collect project data

--- scripts/home/collectProjectData.js
+++ scripts/home/collectProjectData.js
@@ -27,28 +27,30 @@ export default async function collectProjectsData(owner, token, projectTopic) {
       })
       .catch((err) => console.log(err));
 
-    //   add projects to projectsData
+    // add projects to projectsData
     projects &&
-      (await Promise.all().map(async (project) => {
-        if (project.owner.login === owner) {
-          const data = {
-            name: project.name,
-            description: project.description,
-            url: project.html_url,
-            stars: project.stargazers_count,
-            language: project.language,
-            owner: project.owner.login,
-            ownerUrl: project.owner.html_url,
-            ownerAvatar: project.owner.avatar_url,
-            ownerType: project.owner.type,
-            ownerCompany: project.owner.company,
-            ownerLocation: project.owner.location,
-            ownerBlog: project.owner.blog,
-            ownerBio: project.owner.bio,
-          };
-          projectsData.push(data);
-        }
-      }));
+      (await Promise.all(
+        projects.map(async (project) => {
+          if (project.owner.login === owner) {
+            const data = {
+              name: project.name,
+              description: project.description,
+              url: project.html_url,
+              stars: project.stargazers_count,
+              language: project.language,
+              owner: project.owner.login,
+              ownerUrl: project.owner.html_url,
+              ownerAvatar: project.owner.avatar_url,
+              ownerType: project.owner.type,
+              ownerCompany: project.owner.company,
+              ownerLocation: project.owner.location,
+              ownerBlog: project.owner.blog,
+              ownerBio: project.owner.bio,
+            };
+            projectsData.push(data);
+          }
+        })
+      ));
 
     await console.log(projectsData);
 
